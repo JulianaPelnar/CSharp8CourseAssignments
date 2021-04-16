7dfc052b4603df4a629c2a273639c3e6462b4510
JPelnar <39034752+JulianaPelnar@users.noreply.github.com>
2021-04-15 22:34:19 -0300

    Hello World!
    Adding files via upload to start the project.

---
+++ CSharp8CourseAssignments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp8CourseAssignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
