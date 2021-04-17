14bc055c41be3d0bad59fbee953cc2fe04d81d8a
Nicolas Saavedra Rojas <njsr_27@hotmail.com>
2021-04-16 21:45:13 -0300

    First Assignment

--- CSharp8CourseAssignments/Program.cs
+++ CSharp8CourseAssignments/Program.cs
@@ -6,7 +6,36 @@ namespace CSharp8CourseAssignments
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 0; i < 8 ; i++ )
+            {
+                for(int j = 1; j <= 10 ; j++)
+                {
+                    if(i < 3)
+                    {
+                        if(j > 4 && j < 7)
+                        {
+                            continue;
+                        }
+                        Console.Write(j + " ");
+                    } else if (i < 7)
+                    {
+                        if(i == 5 && j > 8)
+                        {
+                            continue;
+                        }
+                        int a = 11 - j;
+                        Console.Write(a + " ");
+                    } else
+                    {
+                        if(j == 8)
+                        {
+                            continue;
+                        }
+                        Console.Write(j + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
